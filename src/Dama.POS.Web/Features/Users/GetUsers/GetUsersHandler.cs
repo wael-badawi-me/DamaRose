2bccfefc2ea025c24775cb67be05c6298d97e03e
waelbadawi <wael.badawi2000@gmail.com>
2024-09-20 15:53:02 +0300

    finish User CRUD

--- src/Dama.POS.Web/Features/Users/GetUsers/GetUsersHandler.cs
+++ src/Dama.POS.Web/Features/Users/GetUsers/GetUsersHandler.cs
@@ -2,14 +2,31 @@
 using Dama.POS.DAL.Entities.Users;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Dama.POS.Web.Features.Users.GetUsers;
 
-public record GetUsersQuery : IRequest<IEnumerable<User>>;
+public record GetUsersQuery(int skip, int take, string search) : IRequest<IEnumerable<User>>;
 
 public class GetUsersHandler(DamaRozeDbContext dbContext) : IRequestHandler<GetUsersQuery, IEnumerable<User>> {
     public async Task<IEnumerable<User>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
     {
-        return await dbContext.Users.ToListAsync(cancellationToken);
+        if (!string.IsNullOrEmpty(request.search))
+        {
+            return await dbContext.Users.Skip(request.skip).Take(request.take).Where(c =>
+                 c.Name.Contains(request.search) ||
+                 c.Password.Contains(request.search) ||
+                 c.IsEnabled.ToString().Contains(request.search)
+                 ).ToListAsync(cancellationToken);
+        }
+        else
+        {
+            return await dbContext.Users.Skip(request.skip).Take(request.take).ToListAsync(cancellationToken);
+        }
     }
 }
+
+
+
+
